09aedadb6f2c51ef990298bb330f904db2a2b18e
gtodur <gtodur@outlook.com>
2020-03-07 10:22:26 +0530

    first commit

---
+++ src/main/java/com/df/springboot/batchprocess/service/SpringBootBatchprocessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.df.springboot.batchprocess.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBatchprocessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBatchprocessServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/df/springboot/batchprocess/service/SpringBootBatchprocessServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.df.springboot.batchprocess.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootBatchprocessServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
